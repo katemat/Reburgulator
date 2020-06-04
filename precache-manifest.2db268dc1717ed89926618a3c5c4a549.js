107d5f10c989e190dce71e7df045618bd005338c
Katerina <matveevakaterinka@gmail.com>
2020-06-04 16:15:09 +1000

    Updates

---
+++ precache-manifest.2db268dc1717ed89926618a3c5c4a549.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58cb97070c26787be0dc252b4d736cd9",
+    "url": "/Reburgulator/index.html"
+  },
+  {
+    "revision": "ab05d8a0adb3a90a7050",
+    "url": "/Reburgulator/static/css/main.2636c269.chunk.css"
+  },
+  {
+    "revision": "4467e00b0b89a5d4d72e",
+    "url": "/Reburgulator/static/js/2.ba0e5091.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Reburgulator/static/js/2.ba0e5091.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab05d8a0adb3a90a7050",
+    "url": "/Reburgulator/static/js/main.3ea306d3.chunk.js"
+  },
+  {
+    "revision": "b3e17dbeaf583d35e9db",
+    "url": "/Reburgulator/static/js/runtime-main.48624c5c.js"
+  }
+]);
